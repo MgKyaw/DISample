85baf8589dae51e3235204cb81c2c72f38bde830
MgKyaw <mgkyaw20112@gmail.com>
2025-01-01 23:22:18 +0700

    feat: Modify GetService for nested parameter instances

--- DependencyResolver.cs
+++ DependencyResolver.cs
@@ -9,21 +9,26 @@ public class DependencyResolver
 
     public T GetService<T>()
     {
-        var dependency = _dependencyContainer.GetDependency(typeof(T));
+        return (T) GetService(typeof(T));
+    }
+
+    public object GetService(Type type)
+    {
+        var dependency = _dependencyContainer.GetDependency(type);
         var constructor = dependency.GetConstructors().Single();
         var parameters = constructor.GetParameters().ToArray();
 
         if (parameters.Length == 0)
         {
-            return (T)Activator.CreateInstance(dependency);
+            return Activator.CreateInstance(dependency);
         }
 
         var parameterImplementations = new Object[parameters.Length];
         for (int i = 0; i < parameters.Length; i++)
         {
-            parameterImplementations[i] = Activator.CreateInstance(parameters[i].ParameterType);
+            parameterImplementations[i] = GetService(parameters[i].ParameterType);
         }
 
-        return (T)Activator.CreateInstance(dependency, parameterImplementations);
+        return Activator.CreateInstance(dependency, parameterImplementations);
     }
 }
