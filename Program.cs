2327e7020087602223a339a7b8cfb7493dafcbe5
MgKyaw <mgkyaw20112@gmail.com>
2025-01-01 21:33:00 +0700

    feat: Apply container and resolver

--- Program.cs
+++ Program.cs
@@ -1,8 +1,16 @@
 ﻿// var helloService = new HelloService();
 // var serviceConsumer = new ServiceConsumer(helloService);
 
-var helloService = (HelloService) Activator.CreateInstance(typeof(HelloService));
-var serviceConsumer = (ServiceConsumer) Activator.CreateInstance(typeof(ServiceConsumer), helloService);
+// var helloService = (HelloService) Activator.CreateInstance(typeof(HelloService));
+// var serviceConsumer = (ServiceConsumer) Activator.CreateInstance(typeof(ServiceConsumer), helloService);
+
+var dependencyContainer = new DependencyContainer();
+dependencyContainer.AddDependency(typeof(HelloService));
+dependencyContainer.AddDependency<ServiceConsumer>();
+
+var dependencyResolver = new DependencyResolver(dependencyContainer);
+
+var helloService = dependencyResolver.GetService<HelloService>();
 
 helloService.Print();
-serviceConsumer.Print();
+//serviceConsumer.Print();
