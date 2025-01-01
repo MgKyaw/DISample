6c3ed8cc4784d49069895531b02c53d9bb810979
MgKyaw <mgkyaw20112@gmail.com>
2025-01-01 21:50:18 +0700

    feat: Modify GetService for parameter instances

--- DependencyResolver.cs
+++ DependencyResolver.cs
@@ -1,7 +1,7 @@
 public class DependencyResolver
 {
     DependencyContainer _dependencyContainer;
-    
+
     public DependencyResolver(DependencyContainer dependencyContainer)
     {
         _dependencyContainer = dependencyContainer;
@@ -9,7 +9,21 @@ public class DependencyResolver
 
     public T GetService<T>()
     {
-        var type = _dependencyContainer.GetDependency(typeof(T));
-        return (T) Activator.CreateInstance(type);
+        var dependency = _dependencyContainer.GetDependency(typeof(T));
+        var constructor = dependency.GetConstructors().Single();
+        var parameters = constructor.GetParameters().ToArray();
+
+        if (parameters.Length == 0)
+        {
+            return (T)Activator.CreateInstance(dependency);
+        }
+
+        var parameterImplementations = new Object[parameters.Length];
+        for (int i = 0; i < parameters.Length; i++)
+        {
+            parameterImplementations[i] = Activator.CreateInstance(parameters[i].ParameterType);
+        }
+
+        return (T)Activator.CreateInstance(dependency, parameterImplementations);
     }
 }
