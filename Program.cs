26f1b02ba67af79fd89df22786849426da3eba03
MgKyaw <mgkyaw20112@gmail.com>
2025-01-01 21:12:34 +0700

    feat: Create instances via Activator

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿var helloService = new HelloService();
-var serviceConsumer = new ServiceConsumer(helloService);
+﻿// var helloService = new HelloService();
+// var serviceConsumer = new ServiceConsumer(helloService);
+
+var helloService = (HelloService) Activator.CreateInstance(typeof(HelloService));
+var serviceConsumer = (ServiceConsumer) Activator.CreateInstance(typeof(ServiceConsumer), helloService);
+
 helloService.Print();
 serviceConsumer.Print();
