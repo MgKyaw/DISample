50903c1a5e698a20d90a531101f506becf3fc8e9
MgKyaw <mgkyaw20112@gmail.com>
2025-01-02 21:49:23 +0700

    feat: Add Dependency.cs

---
+++ Dependency.cs
@@ -0,0 +1,18 @@
+public class Dependency 
+{
+    public Dependency(Type type, DependencyLifetime lifetime)
+    {
+        Type = type;
+        Lifetime = lifetime;
+    }
+    public Type Type { get; set; }
+    public DependencyLifetime Lifetime { get; set; }
+    public object Implementation { get; set; }
+    public bool Implemented { get; set; }
+
+    public void AddImplementation(object i)
+    {
+        Implementation = i;
+        Implemented = true;
+    }
+}
